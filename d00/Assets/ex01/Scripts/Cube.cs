58a1988262026059787abd1ec6998f5bda13c21b
Francois SIDLER <fsidler@e2r9p16.42.fr>
2017-02-09 22:27:06 +0100

    d00

---
+++ d00/Assets/ex01/Scripts/Cube.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+	private float	speed;
+	private int	_framesCount = 15;
+	private float	_dx;
+	private float	_initScale;
+	private float	_targetScale;
+	private bool	_hasToBeDestroyed = false;
+	private bool	_fade_away = false;
+	private Vector4	s_p;
+
+	void	Start () {
+		_initScale = gameObject.transform.localScale.x;
+		_targetScale = _initScale * 1.6f;
+		_dx = (_targetScale - _initScale) / _framesCount;
+		speed = Random.Range (1.7f, 3.0f);
+		s_p = gameObject.GetComponent<SpriteRenderer>().color;
+	}
+
+	void OnBecameInvisible() {
+		Destroy (gameObject);
+	}
+
+	public void	fade_away() {
+		_fade_away = true;
+	}
+
+	public void	switch_status() {
+		_hasToBeDestroyed = true;
+	}
+
+	void	Update () {
+		if (_hasToBeDestroyed) {
+			_initScale += _dx;
+			s_p.w -= 0.1f;
+			if (_initScale > _targetScale) {
+				_initScale = _targetScale;
+				Destroy (gameObject);
+			}
+			gameObject.GetComponent<SpriteRenderer> ().color = s_p;
+			gameObject.transform.localScale = Vector3.one * _initScale;
+		}
+		else {
+			if (_fade_away) {
+				s_p.w -= (0.5f / 10.0f) * speed;
+				gameObject.GetComponent<SpriteRenderer> ().color = s_p;
+			}
+			float dist = Time.deltaTime * speed;
+			transform.Translate (0, -dist, 0);
+		}
+	}
+}
---
+++ d00/Assets/ex01/Scripts/CubeSpawner.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+
+	public int	spawnTime;
+	public static float	timer = 0;
+	public Cube[]	prefabs;
+	private Cube[]	objects = new Cube[3];
+	public Transform[]	obj_begin;
+	public Transform[]	obj_end;
+	private string[]	keycodes = new string[]{"a", "s", "d"};
+	private bool[]	_miss = new bool[]{false, false, false};
+
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= spawnTime) {
+			timer = 0;
+			int rand = Random.Range (0, 7);
+			if (rand < 3) {
+				if (!objects [rand]) {
+					objects [rand] = (Cube)GameObject.Instantiate (prefabs [rand], obj_begin [rand].position, Quaternion.identity);
+					_miss [rand] = false;
+				}
+			} else if (rand == 3) {
+				if (!objects [0] && !objects[1]) {
+					objects [0] = (Cube)GameObject.Instantiate (prefabs [0], obj_begin [0].position, Quaternion.identity);
+					_miss [0] = false;
+					objects [1] = (Cube)GameObject.Instantiate (prefabs [1], obj_begin [1].position, Quaternion.identity);
+					_miss [1] = false;
+				}
+			} else if (rand == 4) {
+				if (!objects [0] && !objects[2]) {
+					objects [0] = (Cube)GameObject.Instantiate (prefabs [0], obj_begin [0].position, Quaternion.identity);
+					_miss [0] = false;
+					objects [2] = (Cube)GameObject.Instantiate (prefabs [2], obj_begin [2].position, Quaternion.identity);
+					_miss [2] = false;
+				}
+			} else if (rand == 5) {
+				if (!objects [1] && !objects[2]) {
+					objects [1] = (Cube)GameObject.Instantiate (prefabs [1], obj_begin [1].position, Quaternion.identity);
+					_miss [1] = false;
+					objects [2] = (Cube)GameObject.Instantiate (prefabs [2], obj_begin [2].position, Quaternion.identity);
+					_miss [2] = false;
+				}
+			} else {
+				if (!objects[0] && !objects [1] && !objects[2]) {
+					objects [0] = (Cube)GameObject.Instantiate (prefabs [0], obj_begin [0].position, Quaternion.identity);
+					_miss [0] = false;
+					objects [1] = (Cube)GameObject.Instantiate (prefabs [1], obj_begin [1].position, Quaternion.identity);
+					_miss [1] = false;
+					objects [2] = (Cube)GameObject.Instantiate (prefabs [2], obj_begin [2].position, Quaternion.identity);
+					_miss [2] = false;
+				}
+			}
+		}
+		int index = 0;
+		while (index < 3) {
+			if (objects [index] && Input.GetKeyDown (keycodes [index]) && _miss[index] == false) {
+				objects [index].switch_status ();
+				Debug.Log ("Precision: " + Mathf.Abs (obj_end [index].position.y - objects [index].transform.localPosition.y));
+			} else if (_miss[index] == false && objects[index] && objects [index].transform.position.y < (obj_end [index].position.y - (objects [index].transform.localScale.y / 2.0f))) {
+				Debug.Log ("Miss!");
+				objects [index].fade_away ();
+				_miss[index] = true;
+			}
+			index += 1;
+		}
+	}
+
+}
