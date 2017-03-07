51d729c58adb676bb317c20761e22ba432936433
Francois SIDLER <fsidler@e2r10p22.42.fr>
2017-03-07 17:38:22 +0100

    d01

---
+++ d01/Assets/ex02/Scripts/playerScript_ex02.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerScript_ex02 : MonoBehaviour {
+
+	public int	playerIndex = 0;
+
+	public float	speed = 2.0f;
+	public float	jump = 5.0f;
+
+	public static int	curIndex = 0;
+
+	public Transform	limits;
+
+	private int	_collisionCount = 0;
+
+	private float	_respawnTime = 0.8f;
+
+	private bool	_respawn = true;
+	private bool	_canJump = false;
+
+	static bool[]	hasReachedExit = new bool[]{ false, false, false };
+
+	private IEnumerator	_coroutine;
+
+	void				Start() {
+		_coroutine = _blink();
+		StartCoroutine (_coroutine);
+	}
+
+	private IEnumerator	_blink() {
+		while (_respawn) {
+			Color	clr = GetComponent<SpriteRenderer> ().color;
+			if (clr.a == 1.0f)
+				clr.a = 0.5f;
+			else
+				clr.a = 1.0f;
+			GetComponent<SpriteRenderer> ().color = clr;
+			yield return new WaitForSeconds (.1f);
+		}
+	}
+
+	void				OnTriggerEnter2D(Collider2D coll) {
+		Color clr = coll.gameObject.GetComponent<SpriteRenderer> ().color;
+		clr.r = 0.12f;
+		clr.g = 1.0f;
+		clr.b = 0.12f;
+		if (coll.gameObject.tag == "red_exit" && playerIndex == 0) {
+			hasReachedExit [0] = true;
+			coll.gameObject.GetComponent<SpriteRenderer> ().color = clr;
+		} else if (coll.gameObject.tag == "yellow_exit" && playerIndex == 1) {
+			hasReachedExit [1] = true;
+			coll.gameObject.GetComponent<SpriteRenderer> ().color = clr;
+		} else if (coll.gameObject.tag == "blue_exit" && playerIndex == 2) {
+			hasReachedExit [2] = true;
+			coll.gameObject.GetComponent<SpriteRenderer> ().color = clr;
+		}
+	}
+
+	void				OnTriggerExit2D(Collider2D coll) {
+		Color clr = coll.gameObject.GetComponent<SpriteRenderer> ().color;
+		clr.r = 1.0f;
+		clr.g = 1.0f;
+		clr.b = 1.0f;
+		if (coll.gameObject.tag == "red_exit" && playerIndex == 0) {
+			hasReachedExit [0] = false;
+			coll.gameObject.GetComponent<SpriteRenderer> ().color = clr;
+		} else if (coll.gameObject.tag == "yellow_exit" && playerIndex == 1) {
+			hasReachedExit [1] = false;
+			coll.gameObject.GetComponent<SpriteRenderer> ().color = clr;
+		} else if (coll.gameObject.tag == "blue_exit" && playerIndex == 2) {
+			hasReachedExit [2] = false;
+			coll.gameObject.GetComponent<SpriteRenderer> ().color = clr;
+		}
+	}
+
+	void				OnCollisionEnter2D(Collision2D coll) {
+		if (coll.enabled && coll.contacts.Length > 0) {
+			_collisionCount++;
+			ContactPoint2D contact = coll.contacts [0];
+			if (Vector2.Dot (contact.normal, Vector2.up) > 0.5)
+				_canJump = true;
+		}
+	}
+
+	void				OnCollisionExit2D(Collision2D coll) {
+		_collisionCount--;
+		if (coll.enabled && _collisionCount == 0)
+			_canJump = false;
+	}
+
+	public void			resetLevel() {
+		SceneManager.LoadSceneAsync ("ex02");
+	}
+
+	public void			loadNextLevel() {
+		SceneManager.LoadSceneAsync ("ex03");
+	}
+
+	void				Update() {
+		if (gameObject.transform.position.y < limits.position.y || Input.GetKeyDown ("r"))
+			resetLevel ();
+		if (_respawn) {
+			if (_respawnTime <= 0) {
+				Color	clr = GetComponent<SpriteRenderer> ().color;
+				clr.a = 1.0f;
+				GetComponent<SpriteRenderer> ().color = clr;
+				StopCoroutine (_coroutine);
+				_respawn = false;
+			}
+			_respawnTime -= Time.deltaTime;
+		}
+		GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.None;
+		GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeRotation;
+		float vertical = GetComponent<Rigidbody2D> ().velocity.y;
+		if (curIndex == playerIndex) {
+			if (_canJump && (Input.GetKey ("space") || Input.GetKey ("up"))) {
+				vertical = jump;
+				_canJump = false;
+			}
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (Input.GetAxis ("Horizontal") * speed, vertical);
+		} else {
+			GetComponent<Rigidbody2D> ().constraints |= RigidbodyConstraints2D.FreezePositionX;
+		}
+		if ((hasReachedExit [0] == true && hasReachedExit [1] == true && hasReachedExit [2] == true) || Input.GetKeyDown ("n"))
+			loadNextLevel ();
+	}
+}
---
+++ d01/Assets/ex04/Scripts/movingPlatformScript_ex04.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D))]
+public class movingPlatformScript_ex04 : MonoBehaviour {
+
+	[Tooltip("horizontal distance in units")]
+	public float	horizontalDistance = 0;
+	[Tooltip("vertical distance in units")]
+	public float	verticalDistance = 0;
+
+	[Tooltip("time it takes to translate the platform over the specified distance")]
+	[Range(1.0f, 5.0f)]
+	public float	time = 1.0f;
+
+	public bool	IsTriggered = false;
+
+	private float	_timer = 0;
+
+	private bool	_movePositive = true;
+
+	private Vector3	_vecMove;
+
+	private Rigidbody2D	_platformRB;
+
+	private GameObject	_player = null;
+
+	void Start () {
+		_platformRB = GetComponent<Rigidbody2D>();
+		_platformRB.isKinematic = true;
+		_vecMove = new Vector3 (horizontalDistance / time, verticalDistance / time, 0);
+	}
+
+	void	OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "player")
+			IsTriggered = false;
+	}
+
+	void	OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "player")
+			_player = coll.gameObject;
+	}
+
+	void	OnCollisionExit2D(Collision2D coll) {
+		if (coll.gameObject.tag == "player")
+			_player = null;
+	}
+
+	void FixedUpdate () {
+		if (IsTriggered == false) {
+			if (_movePositive) {
+				_platformRB.MovePosition (transform.position + _vecMove * Time.deltaTime);
+				if (_player)
+					_player.GetComponent<Rigidbody2D> ().velocity += new Vector2 (horizontalDistance / time, 0);
+				if (_timer >= time) {
+					_timer = 0;
+					_movePositive = false;
+				}
+				_timer += Time.fixedDeltaTime;
+			} else {
+				_platformRB.MovePosition (transform.position - _vecMove * Time.deltaTime);
+				if (_player)
+					_player.GetComponent<Rigidbody2D> ().velocity += new Vector2 (-horizontalDistance / time, 0);
+				if (_timer >= time) {
+					_timer = 0;
+					_movePositive = true;
+				}
+				_timer += Time.fixedDeltaTime;
+			}
+		}
+	}
+}
